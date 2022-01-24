cfc4fb9e537c3657979316c0f3cdd91905dc13e5
satviksriv <srivastavasatvik22@gmail.com>
2022-01-24 20:24:33 +0530

    Added addNote function to the app

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const Yargs = require("yargs/yargs");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -23,8 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title: " + argv.title);
-    console.log("Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
